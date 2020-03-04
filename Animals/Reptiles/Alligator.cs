06ddeef52032884b4a99ff0cfe21b3da76b41baa
Crystal Broach <crysland@gmail.com>
2020-03-03 22:22:44 -0600

    Add project files.

---
+++ Animals/Reptiles/Alligator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Reptiles
+{
+    class Alligator
+    {
+        public bool IsAquatic { get; set; }
+        private int _stepGoal;
+        private int _stepCount;
+
+        public Alligator(int stepGoal)
+        {
+            IsAquatic = true;
+            _stepGoal = stepGoal;
+        }
+        internal void Grunt ()
+        {           
+            if (IsAquatic)
+            {
+                Console.WriteLine("bubble bubble");
+            }
+            else
+            {
+                Console.WriteLine("Grrrrr!");
+            }
+        }
+
+        public void Run(int steps)
+        {
+            _stepCount += steps;
+
+            Console.WriteLine($"You ran {steps} steps.");
+
+            if (_stepCount >= _stepGoal)
+            {
+                Console.WriteLine("You achieved your step goal!");
+            }
+        }
+    }
+}
